5ee880d58352a981d04993e046ce6f9f2b7e3445
Vladimir Albrekht <vladimir_albreht@mail.ru>
2022-11-13 17:34:08 +0800

    Fix mistakes

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,25 @@
-  
 let popupElement = document.querySelector('.popup');
 let EditButton = document.querySelector('.profile__edit-button');
 let CloseButton = document.querySelector('.popup__close');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__status');
-let SaveButton = document.querySelector('.popup__save-button');
 
-let openPopup = function(){
-    popupElement.classList.add('popup_opened')
-}
 
-let closePopup = function () {
+let closePopup = function(){
     popupElement.classList.remove('popup_opened')
 }
 
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_job');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+
+let openPopup = function(){
+    popupElement.classList.add('popup_opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    profileName.textContent = nameInput.value;
+    profileName.textContent = nameInput.value; /* Добавляем значениие из поля инпут во внутрь тега. При повторном открытии попапа данные заносятся из полей профиля в инпуты  */
     profileJob.textContent = jobInput.value;
     popupElement.classList.remove('popup_opened');
 }
