1901b1c2e61400be5e3aa0d2e8c756b5c0550f97
Vladimir Albrekht <vladimir_albreht@mail.ru>
2022-11-15 22:22:30 +0800

    Commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,13 @@
 let popupElement = document.querySelector('.popup');
+let popupForm = document.querySelector('.popup__form');
 let EditButton = document.querySelector('.profile__edit-button');
 let CloseButton = document.querySelector('.popup__close');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__status');
 
-
 let closePopup = function(){
     popupElement.classList.remove('popup_opened')
 }
-
 
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_job');
@@ -19,13 +18,12 @@ let openPopup = function(){
     jobInput.value = profileJob.textContent;
 }
 
-function formSubmitHandler (evt) {
+popupForm.addEventListener('submit', function(evt) {
     evt.preventDefault(); 
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     closePopup();  
-}
+}); 
 
-popupElement.addEventListener('submit', formSubmitHandler); 
 EditButton.addEventListener('click', openPopup);
 CloseButton.addEventListener('click', closePopup);
