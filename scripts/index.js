093fe4ab7c63e3fb892c329f8a5ce7e8040bdde0
Vladimir Albrekht <vladimir_albreht@mail.ru>
2022-12-14 11:47:50 +0800

    Sprint 6

--- scripts/index.js
+++ scripts/index.js
@@ -19,74 +19,6 @@ const profileJob = document.querySelector('.profile__status');
 const nameInput = document.querySelector('[name="popup-name"]');
 const jobInput = document.querySelector('[name="popup-job"]');
 
-
-/* ВАЛИДАЦИЯ ФОРМ */
-
-const checkInputValidity = (input,config) => {
-    const error = document.querySelector(`#${input.id}-error`)
-
-    if(input.validity.valid) {
-        error.textContent= ''
-        error.classList.remove(config.errorClass)
-        input.classList.remove(config.inputErrorClass)
-
-    } else {
-        error.textContent = input.validationMessage
-        error.classList.add(config.errorClass)
-        input.classList.add(config.inputErrorClass)
-    }
-
-}
-
-const toggleButton = (inputs, button, config) => {
-    const isFormValid = inputs.every(input => input.validity.valid)
-
-    if(isFormValid) {
-         button.classList.remove(config.inactiveButtonClass)
-         button.disabled = ''
-    } else {
-        button.classList.add(config.inactiveButtonClass)
-        button.disabled = 'disabled'
-    }
-}
-
-
-const enableValidation = (config) => {
-    const {formSelector, inputSelector, submitButtonSelector, ...restConfig} = config
-    const forms = [...document.querySelectorAll(formSelector)]
-
-    forms.forEach(form => {
-        const inputs = [...form.querySelectorAll(inputSelector) ] 
-        const button = form.querySelector(submitButtonSelector)
-
-        form.addEventListener('submit', (e) => {
-            e.preventDefault()
-        })
-
-
-    inputs.forEach(input => {
-        input.addEventListener('input', () => {
-            checkInputValidity(input, restConfig)
-            toggleButton(inputs, button, config)   
-            })
-        })
-    })
-}
-
-enableValidation({
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_invalid',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_visible'
-})
-
-
-
-
-
-
 /* ФУНКЦИИ */
 
 const openPopup = function(item){
@@ -120,6 +52,29 @@ buttonCloseAddForm .addEventListener('click', function  () {
     closePopup(popupAddCard);
     removeValueformAddCard();
 } );
+
+//ЗАКРЫВАЕМ ПОПАП ЧЕРЕЗ ESCAPE
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape') {
+    //ваша функция закрытия окна
+    closePopup(popupEditProfileContainer)
+    closePopup(popupOpenImage)
+    closePopup(popupAddCard)
+    }
+});
+
+// ЗАКРЫВАЕМ ФОРМУ ЧЕРЕЗ OVERLAY
+
+function closePopupOnOverlayClick (evt) {
+    if (evt.target.classList.contains('popup_opened')) {
+      closePopup(evt.target);
+    }
+  }
+
+  popupEditProfileContainer.addEventListener('click', closePopupOnOverlayClick);
+  popupOpenImage.addEventListener('click', closePopupOnOverlayClick);
+  popupAddCard.addEventListener('click', closePopupOnOverlayClick);
 
 formEditProfile.addEventListener('submit', function(evt) {
     evt.preventDefault(); 
